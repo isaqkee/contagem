4b3e14a3db7011e98676608f661050c00d9be462
isaqkee <isaquecorreia34@gmail.com>
2023-11-13 13:40:37 -0300

    finalizado

---
+++ data.js
@@ -0,0 +1,9 @@
+// var data = new Date().getMonth()+1;
+// console.log(data);
+
+var relogio = setInterval(function () {
+    let horas =new Date().getHours();
+    let minutos =new Date().getMinutes();
+    let segundos =new Date().getSeconds();
+    console.log(`${horas}:${minutos}:${segundos}`);
+}, 1000);
